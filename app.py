931859cc6e811ee2d313c82c8f6280a4ef2298b7
thevikramrajput <vikrammadhad@gmail.com>
2026-04-17 12:11:55 +0530

    Fix 500 Boot TypeError by correctly handling missing Vercel Envs

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ load_dotenv()
 PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
 GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
 
-os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
+if PINECONE_API_KEY:
+    os.environ['PINECONE_API_KEY'] = PINECONE_API_KEY
 if GOOGLE_API_KEY:
     os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
 
